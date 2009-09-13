e624347be69f6771d9f17604ed44a386639bc74b
robinouu <robinouu@2f68549c-7923-11de-adf2-cbd3851e0ccf>
2009-09-13 19:40:48 +0000

    Added basic Icon widget

---
+++ trunk/include/Icon.hpp
@@ -0,0 +1,52 @@
+#ifndef ICON_HPP_INCLUDED
+#define ICON_HPP_INCLUDED
+
+/*
+    This file is part of SFUI (by Robin RUAUX).
+
+    SFUI is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SFUI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SFUI.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "Widget.hpp"
+#include <SFML/Graphics/Sprite.hpp>
+
+namespace sf
+{
+    namespace ui
+    {
+        class Icon : public Widget
+        {
+            public :
+                Icon();
+                Icon(const Sprite& sprite);
+                Icon(const std::string& filename);
+
+                void            SetSprite(const Sprite& sprite);
+                const Sprite&   GetSprite() const;
+
+                virtual void    LoadStyle(const std::string& nameStyle);
+
+            protected :
+                virtual void    OnPaint(RenderTarget& target) const;
+                virtual void    OnChange(Widget::Property property);
+
+            private :
+                Sprite          mSprite;
+        };
+    }
+}
+
+
+#endif // ICON_HPP_INCLUDED
---
+++ trunk/src/Icon.cpp
@@ -0,0 +1,87 @@
+/*
+    This file is part of SFUI (by Robin RUAUX).
+
+    SFUI is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SFUI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SFUI.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "Icon.hpp"
+#include <SFML/Graphics/Image.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+namespace sf
+{
+    namespace ui
+    {
+        Icon::Icon()
+            :   Widget(),
+                mSprite()
+        {
+            SetDefaultStyle("BI_Icon");
+            LoadStyle(GetDefaultStyle());
+        }
+
+        void    Icon::LoadStyle(const std::string& nameStyle)
+        {
+            Widget::LoadStyle(nameStyle);
+
+            ResourceManager* rm = ResourceManager::Get();
+            StyleProperties& properties = rm->GetStyle(nameStyle);
+
+            if (properties["image"] != "")
+            {
+                mSprite = rm->GetImage(properties["image"]);
+
+                const Image* img = mSprite.GetImage();
+
+                if (img && (properties["width"] == "" || properties["height"] == ""))
+                {
+                    SetSize((properties["width"] == "") ? img->GetWidth() : GetWidth(),
+                            (properties["height"] == "") ? img->GetHeight() : GetHeight());
+                }
+                else
+                {
+                    mSprite.Resize(GetWidth(), GetHeight());
+                }
+            }
+        }
+
+        void    Icon::SetSprite(const Sprite& sprite)
+        {
+            mSprite = sprite;
+        }
+
+        const Sprite&   Icon::GetSprite() const
+        {
+            return mSprite;
+        }
+
+        void    Icon::OnChange(Widget::Property property)
+        {
+            if (property == Widget::SIZE)
+            {
+                mSprite.Resize(GetSize());
+            }
+        }
+
+        void    Icon::OnPaint(RenderTarget& target) const
+        {
+            Widget::OnPaint(target);
+
+            if (mSprite.GetImage())
+                target.Draw(mSprite);
+        }
+
+    }
+}
